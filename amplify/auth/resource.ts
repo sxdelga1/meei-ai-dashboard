1e9260d21591920b6a406d41fdf7f0e1125e3d3f
sxdelga1 <sxdelga1@hotmail.com>
2025-11-14 07:19:29 -0500

    Create resource.ts

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,13 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  userAttributes: {
+    fullname: {
+      mutable: true,
+      required: true,
+    },
+  },
+});
